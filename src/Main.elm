40b5c0ab250e91541bcf2e5e0ba1208d404c3984
Adrien Aury <adrienaury@gmail.com>
2023-06-18 19:43:02 +0200

    feat: insert sequence in Main view

--- src/Main.elm
+++ src/Main.elm
@@ -1,31 +1,35 @@
-module Main exposing (main)
+module Main exposing (..)
 
-import Browser
-import HelloWorld exposing (helloWorld)
-import Html exposing (Html, div, img)
-import Html.Attributes exposing (src, style)
-import Msg exposing (Msg(..))
-import VitePluginHelper
+import Css
+import Css.Global
+import Html.Styled as Html
+import Html.Styled.Attributes as Attr
+import Tailwind.Breakpoints as Breakpoints
+import Tailwind.Utilities as Tw
+import Tailwind.Theme as Tw
 
+import View.Field as View
 
-main : Program () Int Msg
 main =
-    Browser.sandbox { init = 0, update = update, view = view }
-
-
-update : Msg -> number -> number
-update msg model =
-    case msg of
-        Increment ->
-            model + 1
-
-        Decrement ->
-            model - 1
-
-
-view : Int -> Html Msg
-view model =
-    div []
-        [ img [ src <| VitePluginHelper.asset "/src/assets/logo.png", style "width" "300px" ] []
-        , helloWorld model
-        ]
+    Html.toUnstyled <|
+        Html.div [ Attr.css [ Tw.bg_color Tw.gray_50 ] ]
+            [ -- This will give us the standard tailwind style-reset as well as the fonts
+              Css.Global.global Tw.globalStyles
+            , Html.div
+                [ Attr.css
+                    [ Tw.flex_row
+                    , Tw.px_5
+                    , Tw.font_sans
+                    , Tw.leading_8
+                    , Tw.text_color Tw.stone_700
+                    , Breakpoints.md
+                        [ Tw.flex
+                        ]
+                    ]
+                ]
+                [ Html.section []
+                    [
+                        View.fieldSequence
+                    ]
+                ]
+            ]
